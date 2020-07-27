a484e89d82c4edc11877f43946062bf4e43a53ca
Rohan Khurana <rohankhdbzmf@gmail.com>
2020-07-27 18:07:39 +0530

    Implementation

---
+++ Tic Tac Toe Game/Assets/DataFiles/Scripts/GridSpace.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public TextMeshProUGUI buttonText;
+    //public string playerSide;
+    private GameController gameController;
+
+
+    public void SetSpace()
+    {
+        if (gameController.playerMove == true)
+        {
+            buttonText.SetText(text: gameController.GetPlayerSide());
+            button.interactable = false;
+            gameController.EndTurn();
+        }
+
+    }
+
+    public void SetGameControllerReference(GameController controller) => gameController = controller;
+}
